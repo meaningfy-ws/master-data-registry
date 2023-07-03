023caa6c3e9eb8f8312cc6d4be1ebc1df34aa23c
CaptainOfHacks <39195263+CaptainOfHacks@users.noreply.github.com>
2023-07-03 20:18:30 +0300

    fix splink tables linking

--- master_data_registry/adapters/registry_manager.py
+++ master_data_registry/adapters/registry_manager.py
@@ -92,17 +92,14 @@ class DuckDBRegistryManager(RegistryManagerABC):
                                                                                       threshold_match_probability=threshold_match_probability)
 
         minimized_clusters = self.__minimize_cluster_records(data=deduplicated_data_clusters)
-        print(minimized_clusters)
         if self.duckdb_adapter.check_if_table_exists(table_name=self.registry_table_name):
             linked_clusters = self.linker_engine.link_records(data=minimized_clusters,
                                                               reference_table_name=self.registry_table_name,
                                                               threshold_match_probability=threshold_match_probability)
-            print(linked_clusters)
             linked_unique_ids = linked_clusters[UNIQUE_ID_L_COLUMN_NAME].unique().tolist()
             unlinked_clusters = minimized_clusters[~minimized_clusters[UNIQUE_ID_COLUMN_NAME].isin(linked_unique_ids)]
             result_linked_data_pairs = linked_clusters[[UNIQUE_ID_L_COLUMN_NAME, UNIQUE_ID_R_COLUMN_NAME,
                                                         MATCH_PROBABILITY_COLUMN_NAME]].copy()
-            print(result_linked_data_pairs)
             if len(unlinked_clusters) > 0:
                 self.duckdb_adapter.insert_dataframe(table_name=self.registry_table_name, data=unlinked_clusters)
                 result_unlinked_data_pairs = self.__get_pairs_from_unlinked_data(unlinked_data=unlinked_clusters)
--- tests/test_data/__init__.py
+++ tests/test_data/__init__.py
@@ -4,4 +4,5 @@ import pathlib
 TEST_DATA_PATH = pathlib.Path(__file__).parent.resolve()
 
 ORGANIZATIONS_DUPLICATES_SAMPLE_PATH = TEST_DATA_PATH / "organizations_duplicates_sample.parquet"
+TEST_ORGANIZATIONS_DUPLICATES_SAMPLE_PATH = TEST_DATA_PATH / "data_table.csv"
 
